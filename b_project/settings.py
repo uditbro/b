5b5da5c2be1191244ff31aef581c5fc869ead770
Dev Poonia <110079684+uditbro@users.noreply.github.com>
2025-02-23 23:51:16 +0530

    Update settings.py

--- b_project/settings.py
+++ b_project/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+CSRF_TRUSTED_ORIGINS = ["https://daddydjango.up.railway.app"]
 
 
 # Quick-start development settings - unsuitable for production
